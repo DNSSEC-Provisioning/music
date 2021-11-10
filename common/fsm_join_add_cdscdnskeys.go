1577b75f66b1277b8b7e96e6d2a1b9d8fe63cd83
Jerry Lundström <lundstrom.jerry@gmail.com>
2021-11-10 14:52:07 +0100

    FSM join Add CDS/CDNSKEYs
    - `fsm_join_add_cdscdnskeys`: Implement logic for checking that DNSKEYs are in sync and then add CDS/CDNSKEY record sets

--- common/fsm_join_add_cdscdnskeys.go
+++ common/fsm_join_add_cdscdnskeys.go
@@ -1,10 +1,119 @@
 package music
 
+import (
+    // "fmt"
+    "log"
+
+    "github.com/miekg/dns"
+)
+
+func fsmJoinAddCdscdnskeys(z *Zone) bool {
+    dnskeys := make(map[string][]*dns.DNSKEY)
+
+    log.Printf("Verifying that DNSKEYs for %s are in sync in group %s", z.Name, z.sgroup.Name)
+
+    for _, s := range z.sgroup.SignerMap {
+        m := new(dns.Msg)
+        m.SetQuestion(z.Name, dns.TypeDNSKEY)
+
+        c := new(dns.Client)
+        r, _, err := c.Exchange(m, s.Address+":53") // TODO: add DnsAddress or solve this in a better way
+
+        if err != nil {
+            log.Printf("%s: Unable to fetch DNSKEYs from %s: %s", z.Name, s.Name, err)
+            return false
+        }
+
+        dnskeys[s.Name] = []*dns.DNSKEY{}
+        for _, a := range r.Answer {
+            dnskey, ok := a.(*dns.DNSKEY)
+            if !ok {
+                continue
+            }
+
+            dnskeys[s.Name] = append(dnskeys[s.Name], dnskey)
+        }
+
+        if len(dnskeys[s.Name]) > 0 {
+            log.Printf("%s: Fetched DNSKEYs from %s:", z.Name, s.Name)
+            for _, k := range dnskeys[s.Name] {
+                if f := k.Flags & 0x101; f == 256 {
+                    log.Printf("%s: - %s (ZSK)", z.Name, k.PublicKey)
+                } else {
+                    log.Printf("%s: - %s (KSK)", z.Name, k.PublicKey)
+                }
+            }
+        } else {
+            log.Printf("%s: No DNSKEYs found in %s", z.Name, s.Name)
+        }
+    }
+
+    // for each signer, check every other_signer if it's missing signer's DNSKEYs
+    all_found := true
+    for signer, keys := range dnskeys {
+        for _, key := range keys {
+            if f := key.Flags & 0x101; f == 256 { // only process ZSK's
+                for other_signer, other_keys := range dnskeys {
+                    if other_signer == signer {
+                        continue
+                    }
+
+                    found := false
+                    for _, other_key := range other_keys {
+                        if other_key.PublicKey == key.PublicKey {
+                            // if other_key.Protocol != key.Protocol {
+                            //     *output = append(*output, fmt.Sprintf("Found DNSKEY in %s but missmatch Protocol: %s", other_signer, key.PublicKey))
+                            //     break
+                            // }
+                            // if other_key.Algorithm != key.Algorithm {
+                            //     *output = append(*output, fmt.Sprintf("Found DNSKEY in %s but missmatch Protocol: %s", other_signer, key.PublicKey))
+                            //     break
+                            // }
+                            found = true
+                            break
+                        }
+                    }
+
+                    if !found {
+                        log.Printf("%s: Still missing %s's DNSKEY %s in %s", z.Name, signer, key.PublicKey, other_signer)
+                        all_found = false
+                    }
+                }
+            }
+        }
+    }
+    if !all_found {
+        return false
+    }
+    log.Printf("%s: All DNSKEYs synced between all signers, creating CDS/CDNSKEY record sets", z.Name)
+
+    // Create CDS/CDNSKEY records sets
+    cdses := []dns.RR{}
+    cdnskeys := []dns.RR{}
+    for _, keys := range dnskeys {
+        for _, key := range keys {
+            if f := key.Flags & 0x101; f == 257 {
+                cdses = append(cdses, key.ToDS(dns.SHA256).ToCDS())
+                cdnskeys = append(cdnskeys, key.ToCDNSKEY())
+            }
+        }
+    }
+
+    for _, signer := range z.sgroup.SignerMap {
+        updater := GetUpdater(signer.Method)
+        if err := updater.Update(&signer, z.Name, &[][]dns.RR{cdses, cdnskeys}, nil); err != nil {
+            log.Printf("%s: Unable to update %s with CDS/CDNSKEY record sets: %s", z.Name, signer.Name, err)
+            return false
+        }
+        log.Printf("%s: Update %s successfully with CDS/CDNSKEY record sets", z.Name, signer.Name)
+    }
+
+    z.StateTransition(FsmStateSyncDnskeys, FsmStateAddCdscdnskeys)
+    return true
+}
+
 var FsmJoinAddCdscdnskeys = FSMTransition{
     Description: "Once all DNSKEYs are present in all signers (criteria), build CDS/CDNSKEYs RRset and push to all signers (action)",
     Criteria:    func(z *Zone) bool { return true },
-    Action: func(z *Zone) bool {
-        z.StateTransition(FsmStateSyncDnskeys, FsmStateAddCdscdnskeys)
-        return true
-    },
+    Action:      fsmJoinAddCdscdnskeys,
 }
