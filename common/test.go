c3106635f7adf47b8586b440df778768c9a16c83
Johan Stenstam <johan.stenstam@internetstiftelsen.se>
2021-12-02 13:43:26 +0100

    test

---
+++ common/test.go
@@ -0,0 +1,14 @@
+//
+// Johan Stenstam, johan.stenstam@internetstiftelsen.se
+//
+package music
+
+type TestConfig struct {
+        Debug   bool
+        Verbose bool
+}
+
+type TestUpdate struct {
+        Type string
+}
+
